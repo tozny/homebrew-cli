2899e72a2feef2b0f8c3cd5461e52b63953164d6
Eric Mann <eric@tozny.com>
2017-05-09 14:18:46 -0700

    Bump 1.0.1

--- cli.rb
+++ cli.rb
@@ -1,8 +1,8 @@
 class Cli < Formula
   desc "commandline client for Tozny e3db"
   homepage "https://github.com/tozny/e3db"
-  url "https://github.com/tozny/homebrew-e3db/releases/download/1.0.0/e3db-1.0.0.tar.gz"
-  sha256 "5622249353cfd3908d999a35bee2de1b65621bb0f24a49fd848785dc1552c61f"
+  url "https://github.com/tozny/homebrew-e3db/releases/download/1.0.1/e3db-1.0.1.tar.gz"
+  sha256 "b0924e5b8db10741b00a86f736f1698d1e4a3853bf7dffe13f05200eb145c1b6"
 
   bottle :unneeded
 
