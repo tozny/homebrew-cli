adb4d9c01d9e45465cb162e9e526120dd0ed4cab
toznyjenkins <info@tozny.com>
2017-11-08 11:44:29 -0800

    Update release binary

--- cli.rb
+++ cli.rb
@@ -1,8 +1,8 @@
 class Cli < Formula
   desc "commandline client for Tozny e3db"
   homepage "https://github.com/tozny/e3db"
-  url "https://github.com/tozny/homebrew-e3db/releases/download/2.0.0/e3db-2.0.0.tar.gz"
-  sha256 "5bc53ab7cd31edcc9d75a9bc52ff09660aea9c0b4f921394c474a24127096190"
+  url "https://github.com/tozny/homebrew-e3db/releases/download/2.1.0/e3db-2.1.0.tar.gz"
+  sha256 "79339d110aa7420475599e126e00427fbf1cecd0174e12be6bb97fbf4ac1438c"
 
   bottle :unneeded
 
